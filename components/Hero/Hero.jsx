8e6c98339dd9ebdc8111d238d296e009b08558b5
Amirreza <95163272+shadow1375@users.noreply.github.com>
2023-02-10 05:14:24 +0330

    Fix bugs

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = ({ heading, message }) => {
   return ( 
     <div id='home' className="flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img">
       {/* overlay */}
-      <div className="absolute h-full w-full top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]"/>
+      <div className="absolute h-screen w-full top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]"/>
       <div className="p-10 text-white z-[2] mt-[-10rem]">
         <h2 className="text-5xl font-bold">{heading}</h2>
         <p className="py-5 text-xl">{message}</p>
